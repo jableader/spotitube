b1b1c25cb09350dc8a1d224c56f9ca7f715ab0a7
unknown <jacobdunk@gmail.com>
2018-04-25 15:23:01 +1000

    First working call

--- poc.py
+++ poc.py
@@ -1,10 +1,22 @@
-import requests
+import requests, base64, json
+
+import secrets
 
 def get_token(clientid, clientsecret):
-    requests.post('https://accounts.spotify.com/api/token', data = { \
-        'grant_type': 'authorization_code', \
-        'code':  # TODO
-        'redirect_uri':
-    })
+    creds = bytearray(clientid + ':' + clientsecret, 'ansi')
+    headers = {'Authorization': 'Basic ' + str(base64.b64encode(creds))[2:-1] }
+    data = { 'grant_type': 'client_credentials' }
+    res = requests.post('https://accounts.spotify.com/api/token', headers=headers, data=data)
+    res.raise_for_status()
 
-resp = requests.get('https://api.spotify.com/v1/users/spotify/playlists/37i9dQZF1DX5WTH49Vcnqp')
+    return res.json()['access_token']
+
+def add_token(token, **headers):
+    return { 'Authorization': 'Bearer ' + token, **headers}
+
+def get_tracks(token, userId, playlistId):
+    resp = requests.get('https://api.spotify.com/v1/users/%s/playlists/%s' %(userId, playlistId), headers=add_token(token))
+    
+
+token = get_token(secrets.CLIENT_ID, secrets.CLIENT_SECRET)
+resp = print(resp.text)
