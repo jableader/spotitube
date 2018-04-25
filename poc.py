f8eb6506b04804ba31e9aa03f5e60098f94097b6
unknown <jacobdunk@gmail.com>
2018-04-25 14:38:24 +1000

    Added secrets.py to gitignore

--- poc.py
+++ poc.py
@@ -1,3 +1,10 @@
 import requests
 
+def get_token(clientid, clientsecret):
+    requests.post('https://accounts.spotify.com/api/token', data = { \
+        'grant_type': 'authorization_code', \
+        'code':  # TODO
+        'redirect_uri':
+    })
+
 resp = requests.get('https://api.spotify.com/v1/users/spotify/playlists/37i9dQZF1DX5WTH49Vcnqp')
